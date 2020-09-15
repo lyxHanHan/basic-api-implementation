bce3088e9aae93058005a971ca6f17dd1e169101
liyixuan <13069579956@163.com>
2020-09-16 05:56:00 +0800

    routing homework

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -7,5 +10,21 @@ import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<String> rsList = Arrays.asList("第一条事件","第二条事件","第三条事件");
+
+  //  @GetMapping("/rs/list")
+//    public String getRslist(){
+//     return rsList.toString();
+//  }
+  @GetMapping("/rs/{index}")
+  public  String getOneRsEvent(@PathVariable int index){
+    return rsList.get(index -1);
+  }
+  @GetMapping("/rs/list")
+  public String getRsEventBetween(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
+    if(start == null || end == null){
+      return rsList.toString();
+    }
+    return  rsList.subList(start-1,end).toString();
+  }
 }
